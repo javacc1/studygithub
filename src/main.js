7b49ec87e9d40a7b74f0ac782cdb7cd090010ca0
xzm <2277316726@qq.com>
2020-09-04 16:27:25 +0800

    '第三次提交'

--- src/main.js
+++ src/main.js
@@ -77,7 +77,12 @@ request1({url:'/home/data'}).then(res=>{
 }).catch(err=>{
     console.log(err);
 })*/
-
+/*//3使用promise封装的axios
+request1({url:'/home/data'}).then(res=>{
+    console.log(res);
+}).catch(err=>{
+    console.log(err);
+})*/
 
 request2({url:'/home/data'}).then(res=>{
     console.log(res);
